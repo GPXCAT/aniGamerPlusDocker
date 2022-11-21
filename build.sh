813f02239c4ba56b41ed77c970daa72ff5c87955
John <john.chang@starnic.com.tw>
2022-11-21 14:11:55 +0800

    build with ver

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ rm -rf "$WORK_DIR/SourceRepository"
 git clone https://github.com/miyouzi/aniGamerPlus.git "$WORK_DIR/SourceRepository"
 cp -f "$WORK_DIR/SourceRepository/sn_list-sample.txt" "$WORK_DIR/sn_list-sample.txt"
 [ ! -f "$WORK_DIR/config.json" ] && cp -f "$WORK_DIR/SourceRepository/config-sample.json" "$WORK_DIR/config.json"
+sed -i 's/127.0.0.1/0.0.0.0/g' config.json
 [ ! -f "$WORK_DIR/sn_list.txt" ] && touch "$WORK_DIR/sn_list.txt"
 [ ! -f "$WORK_DIR/aniGamer.db" ] && touch "$WORK_DIR/aniGamer.db"
 [ ! -f "$WORK_DIR/cookie.txt" ] && touch "$WORK_DIR/cookie.txt"
