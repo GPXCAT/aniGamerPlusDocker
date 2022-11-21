1d8de8ab082a769b40ffc98843ba55386b0909d4
John <john.chang@starnic.com.tw>
2022-11-21 14:00:49 +0800

    test

--- build.sh
+++ build.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 export WORK_DIR=$PWD
 
-rm -rf "$PWD/SourceRepository"
-git clone https://github.com/miyouzi/aniGamerPlus.git "$PWD/SourceRepository"
-rm -f "$PWD/SourceRepository/Dockerfile"
-rm -f "$PWD/SourceRepository/docker-compose.yml"
-cp -f "$PWD/SourceRepository/.dockerignore" "$PWD/.dockerignore"
-cp -f "$PWD/SourceRepository/sn_list-sample.txt" "$PWD/sn_list-sample.txt"
-[ ! -f "$PWD/config.json" ] && cp -f "$PWD/SourceRepository/config-sample.json" "$PWD/config.json"
-[ ! -f "$PWD/sn_list.txt" ] && touch "$PWD/sn_list.txt"
-[ ! -f "$PWD/aniGamer.db" ] && touch "$PWD/aniGamer.db"
-[ ! -f "$PWD/cookie.txt" ] && touch "$PWD/cookie.txt"
+rm -rf "$WORK_DIR/SourceRepository"
+git clone https://github.com/miyouzi/aniGamerPlus.git "$WORK_DIR/SourceRepository"
+cp -f "$WORK_DIR/SourceRepository/.dockerignore" "$WORK_DIR/.dockerignore"
+cp -f "$WORK_DIR/SourceRepository/sn_list-sample.txt" "$WORK_DIR/sn_list-sample.txt"
+[ ! -f "$WORK_DIR/config.json" ] && cp -f "$WORK_DIR/SourceRepository/config-sample.json" "$WORK_DIR/config.json"
+[ ! -f "$WORK_DIR/sn_list.txt" ] && touch "$WORK_DIR/sn_list.txt"
+[ ! -f "$WORK_DIR/aniGamer.db" ] && touch "$WORK_DIR/aniGamer.db"
+[ ! -f "$WORK_DIR/cookie.txt" ] && touch "$WORK_DIR/cookie.txt"
 
-cd "$PWD/SourceRepository"
+cd "$WORK_DIR/SourceRepository"
 export APP_TAG=$(git describe --tags --abbrev=0)-$(git rev-parse --short HEAD)
-cd "$PWD"
-echo "APP_TAG=${APP_TAG}" > $PWD/.env
-echo "ani-gamer-plus:${APP_TAG}"
-docker-compose build --no-cache
+cd "$WORK_DIR"
+docker build -t ani-gamer-plus:latest --no-cache -t ani-gamer-plus:${APP_TAG} .
